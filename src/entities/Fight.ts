8bc17a95e8545fd71c631da130f94e997f0509b0
8devabhi <abhinava.dev@gmail.com>
2023-09-07 20:09:01 +0530

    api bulk commit

---
+++ src/entities/Fight.ts
@@ -0,0 +1,32 @@
+import { Entity, PrimaryGeneratedColumn, ManyToOne, JoinTable, OneToOne } from "typeorm";
+import { Fighter } from "./Fighter";
+import { Event } from "./Event";
+
+@Entity()
+export class Fight {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  winType!:string;
+
+  @ManyToOne(() => Fighter)
+  @JoinTable()
+  contestant1!: Fighter;
+
+  @ManyToOne(() => Fighter)
+  @JoinTable()
+  contestant2!: Fighter;
+
+  @ManyToOne(() => Fighter, (fighter) => fighter.wonFights)
+  @JoinTable()
+  winner?: Fighter;
+
+  @ManyToOne(() => Fighter, (fighter) => fighter.lostFights)
+  @JoinTable()
+  loser?: Fighter;
+
+  @ManyToOne(() => Event, (event) => event.fights)
+  // @JoinTable()
+  event?: Event;
+
+}
